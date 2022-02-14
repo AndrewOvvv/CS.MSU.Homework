e01cbf133e41b6eee0e8b29f54dacf12396a47b0
AndrewOvvv <63587191+AndrewOvvv@users.noreply.github.com>
2022-02-14 12:35:24 +0300

    Update generator.c

--- generator.c
+++ generator.c
@@ -4,25 +4,30 @@
 
 enum {DIFF_SIZES = 4};
 
+
+long long mabs(long long x)
+{
+    if (x < 0)
+        return -x;
+    return x;
+}
+
+
+int compare_inc(const void * x1, const void * x2)
+{
+    return mabs(*(int*)x1) >= mabs(*(int*)x2);
+}
+
+
+int compare_dec(const void * x1, const void * x2)
+{
+    return mabs(*(int*)x1) <= mabs(*(int*)x2);
+}
+
+
 long long trand() //function generates truely random nauber which can be lower than zero
 {
     return rand() - RAND_MAX / 2;
-}
-
-
-void gen_inc(long long* array, int size) //function generates a strictly increasing sequence of numbers
-{
-    array[0] = trand();
-    for (int i = 1; i < size; i++)
-        array[i] = array[i - 1] + rand();
-}
-
-
-void gen_dec(long long* array, int size) // the function generates a strictly decreasing sequence of numbers
-{
-    array[0] = trand();
-    for (int i = 1; i < size; i++)
-        array[i] = array[i - 1] - rand();
 }
 
 
@@ -30,6 +35,20 @@ void gen_rnd(long long* array, int size) // the function generates a random sequ
 {
     for (int i = 0; i < size; i++)
         array[i] = trand();
+}
+
+
+void gen_inc(long long* array, int size) //function generates a strictly increasing sequence of numbers
+{
+    gen_rnd(array, size);
+    qsort(array, size, sizeof(long long), compare_inc);
+}
+
+
+void gen_dec(long long* array, int size) // the function generates a strictly decreasing sequence of numbers
+{
+    gen_rnd(array, size);
+    qsort(array, size, sizeof(long long), compare_dec);
 }
 
 
